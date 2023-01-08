9411017c8abe87c6a634bc87bc8bbb5dc0d369bd
Juuso Äikäa <aikjuu89@outlook.com>
2023-01-08 13:56:11 +0200

    Lisätty static middleware

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('postData', (req) => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 
 let persons = [
